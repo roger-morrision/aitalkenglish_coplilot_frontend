bc37e48e0519ed0d1a95bb914d3dd7369ed7bb8e
Roger <viettuan.tran144@gmail.com>
2025-07-25 10:34:13 +0000

    feat: Add production web build for GitHub Pages deployment
    • Updated .gitignore to allow build/web folder for public hosting
    • Built Flutter web app for production deployment
    • Prepared app for GitHub Pages hosting at build/web path

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4193933255"
+    serviceWorkerVersion: "2807880150"
   }
 });
